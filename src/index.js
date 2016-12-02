aadb3773f1cb8d5d74b6bbb4bc0dff95d34222f4
Alex Kabbach <akabbach@gmail.com>
2016-12-02 15:54:37 +0100

    v1.1.0 Added TMPattern model

--- src/index.js
+++ src/index.js
@@ -1,64 +1,17 @@
-export {
-  default as AnnotationSet,
-}
-from './models/annotationSet';
-export {
-  default as Corpus,
-}
-from './models/corpus';
-export {
-  default as Document,
-}
-from './models/document';
-export {
-  default as Frame,
-}
-from './models/frame';
-export {
-  default as FrameElement,
-}
-from './models/frameElement';
-export {
-  default as FERelation,
-}
-from './models/feRelation';
-export {
-  default as FrameRelation,
-}
-from './models/frameRelation';
-export {
-  default as FrameRelationType,
-}
-from './models/frameRelationType';
-export {
-  default as Label,
-}
-from './models/label';
-export {
-  default as Lexeme,
-}
-from './models/lexeme';
-export {
-  default as LexUnit,
-}
-from './models/lexUnit';
-export {
-  default as Pattern,
-}
-from './models/pattern';
-export {
-  default as SemType,
-}
-from './models/semType';
-export {
-  default as Sentence,
-}
-from './models/sentence';
-export {
-  default as ValenceUnit,
-}
-from './models/valenceUnit';
-export {
-  default as Set,
-}
-from './utils/utils';
+export { default as AnnotationSet } from './models/annotationSet';
+export { default as Corpus } from './models/corpus';
+export { default as Document } from './models/document';
+export { default as Frame } from './models/frame';
+export { default as FrameElement } from './models/frameElement';
+export { default as FERelation } from './models/feRelation';
+export { default as FrameRelation } from './models/frameRelation';
+export { default as FrameRelationType } from './models/frameRelationType';
+export { default as Label } from './models/label';
+export { default as Lexeme } from './models/lexeme';
+export { default as LexUnit } from './models/lexUnit';
+export { default as Pattern } from './models/pattern';
+export { default as SemType } from './models/semType';
+export { default as Sentence } from './models/sentence';
+export { default as TMPattern } from './models/tmpattern';
+export { default as ValenceUnit } from './models/valenceUnit';
+export { default as Set } from './utils/utils';
